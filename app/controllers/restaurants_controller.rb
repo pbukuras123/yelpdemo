1e201522efb324e0183648712961afe5a6d4a0f3
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2020-03-14 10:38:05 -0400

    Add custom css

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -69,6 +69,6 @@ class RestaurantsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :phone, :websit, :image)
+      params.require(:restaurant).permit(:name, :address, :phone, :website, :image)
     end
 end
