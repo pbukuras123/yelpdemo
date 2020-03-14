7663f4a3f46183e4cbc9de819013abdc7b607d3b
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2020-03-14 15:01:45 -0400

    Link reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_183907) do
+ActiveRecord::Schema.define(version: 2020_03_14_185546) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_03_14_183907) do
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
