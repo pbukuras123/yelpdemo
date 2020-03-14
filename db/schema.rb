68ed6c9ba65f5776427d0f94c458ef2ac85e2223
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2020-03-14 15:56:43 -0400

    Link reviews with restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_185546) do
+ActiveRecord::Schema.define(version: 2020_03_14_193844) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_03_14_185546) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
